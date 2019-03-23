872c481f03c5d6997189dd2b884532bc14efba81
何大洋 <he.jinyang@foxmail.com>
2019-03-24 00:18:53 +0800

    19/03/23

--- 【058】最后一个单词的长度/src/Solution.java
+++ 【058】最后一个单词的长度/src/Solution.java
@@ -13,17 +13,33 @@
 
 public class Solution {
     public int lengthOfLastWord(String s) {
-        String[] strings = s.split(" ");
-        int max = 0;
-        for (String i: strings) {
-            max = Math.max(max,i.length());
+        //代表没有" "
+        if (s.indexOf(" ") == -1) {
+            return s.length();
         }
-        return max;
+        //拆分
+        String[] strings = s.split(" ");
+        int last = strings.length-1;
+        //说明为 " " 情况  没有单词
+        if (last < 0) {
+            return 0;
+        } else {
+            return strings[last].length();
+        }
+    }
+    /**
+     * 看到的好方法:
+     * 先去掉前后空格，然后找到最后一个空格后的单词第一个位置
+     */
+
+    public int lengthOfLastWord2(String s) {
+        s.trim();
+        return s.substring(s.lastIndexOf(" ")+1).length();
     }
 
 
     public static void main(String[] args) {
-        int ans = new Solution().lengthOfLastWord("Hello World");
+        int ans = new Solution().lengthOfLastWord("as");
         System.out.println(ans);
     }
 }
