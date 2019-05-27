99bad207285c782075dd5a0559f07b980fa307cf
何大洋 <he.jinyang@foxmail.com>
2019-05-27 22:20:32 +0800

    19/05/27

---
+++ 【108】将有序数组转换为二叉搜索树/src/Solution.java
@@ -0,0 +1,39 @@
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 示例:
+ * 给定有序数组: [-10,-3,0,5,9],
+ * 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+ *
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ */
+public class Solution {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        if (nums == null) {
+            return null;
+        }
+        return buildTree(nums, 0, nums.length - 1);
+    }
+
+
+    public TreeNode buildTree(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int m = l + (r - l) / 2;
+        TreeNode treeNode = new TreeNode(nums[m]);
+        treeNode.left = buildTree(nums, l, m - 1);
+        treeNode.right = buildTree(nums, m + 1, r);
+        return treeNode;
+    }
+}
