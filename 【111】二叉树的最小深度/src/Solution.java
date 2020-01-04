0c2f221685e9f6654e6f350e427365b5fd158a90
hjy <543363559@qq.com>
2020-01-04 16:34:40 +0800

    2020.1.4

---
+++ 【111】二叉树的最小深度/src/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min_depth = Math.min(minDepth(root.left), min_depth);
+        }
+
+        if (root.right != null) {
+            min_depth = Math.min(minDepth(root.right), min_depth);
+        }
+        return min_depth+1;
+    }
+}
---
+++ 【111】二叉树的最小深度/src/TreeNode.java
@@ -0,0 +1,13 @@
+import sun.reflect.generics.tree.Tree;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
